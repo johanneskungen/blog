54836e6025a022db61b3e478e32339ddda8f4e7a
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-06-01 17:37:17 +0200

    mobile friendly

--- components/CreateButton.jsx
+++ components/CreateButton.jsx
@@ -1,21 +1,49 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import { createPost } from '../_actions'
+import React, { useState } from "react";
+import { createPost } from "../_actions";
+import { Toaster, toast } from "react-hot-toast";
 
 function CreateButton(user) {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
 
-    async function runAction(){
-        const post = await createPost(input, user)
+  async function runAction() {
+    if(!input || input === "") {
+      toast.error("Input field is empty", {
+        duration: 3000,
+        position: "top-center"
+      })
+      return
     }
+    const post = await createPost(input, user);
+    if (!post) return null;
+
+    setInput("");
+    toast.success("Post was created", {
+      duration: 3000,
+      position: "top-center",
+    });
+  }
 
   return (
-    <form action={runAction}>
-      <input value={input} onChange={e => setInput(e.target.value)} placeholder='post' className='border px-3 py-2.5 text-sm w-72 rounded shadow-lg'/>
-        <button type='submit' className='bg-blue-500 text-white px-2 py-2.5 rounded shadow-lg text-sm'>create post</button>
-    </form>
-  )
+    <>
+      <Toaster />
+      <form className="flex flex-col gap-1" action={runAction}>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Title"
+          className="border px-3 py-2.5 focus:outline-blue-500 text-xs md:text-sm w-72 rounded shadow-lg"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-xs text-white w-[7rem] px-3 py-2.5 rounded shadow-lg md:text-sm"
+        >
+          create post
+        </button>
+      </form>
+    </>
+  );
 }
 
-export default CreateButton
+export default CreateButton;
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -6,20 +6,22 @@ async function Posts({ user }) {
   const posts = await findAllPosts();
   const users = await findAllAction();
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {posts?.map((post) => (
         <div
-          className="border p-4 shadow-lg rounded justify-between flex flex-col gap-8 w-72 h-80 text-sm"
+          className="border p-4 shadow-lg rounded justify-between flex flex-col gap-8 w-72 max-h-80 text-sm"
           key={post.id}
         >
           <div>
-          <p className="text-base font-bold">{post.title}</p>
-          <p className="font-semibold text-gray-700">
-            @{users.find((user) => user.id === posts[0].author_id).name}
-          </p>
+            <p className="text-base font-bold">{post.title}</p>
+            <p className="font-semibold text-gray-700">
+              @{users.find((user) => user.id === posts[0].author_id).name}
+            </p>
           </div>
           <div className="flex border bg-gray-200 text-sm items-center rounded shadow-lg py-0.5 px-1 w-fit">
-            <p>{post.liked.length}</p>
+            <p className="text-sm text-gray-700 font-semibold">
+              {post.liked.length}
+            </p>
             <LikeButton user={user} post={post} />
           </div>
         </div>
